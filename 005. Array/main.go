2db7b5c7e7f53f75250c772b1c429c80a065cd86
PlanB <city7310@naver.com>
2018-01-02 10:56:35 +0900

    Array

--- 005. Array/main.go
+++ 005. Array/main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 )
 
-// Formatted I/O를 위해
-
 func main() {
+	// Go의 배열은 동일한 타입의 원소가 연속된 블록에 저장되는 fixed-length 데이터 타입
+
 	var arr [5]int
 	// 1차원
 	arr[0] = 3
 	fmt.Println(arr)
 
-	arr2 := [5]int{1, 2, 3, 4, 5}
+	arr2 := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(arr2)
 	fmt.Println(len(arr2))
-	// length
+	// 리터럴로 array 선언. length는 추론됨
 
-	arr3 := [][]int{{1, 2}, {3}}
-	// 2차원. 선언과 함께 값 할당 시 array length는 생략 가능
+	arr3 := [4][2]int{1: {1, 2}, 3: {3}}
+	// 특정 요소만 초기화
 	fmt.Println(arr3)
 }
