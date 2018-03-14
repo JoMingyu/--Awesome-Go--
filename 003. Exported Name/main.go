3704214fe76c0f2e648d556b672c132d80743292
JoMingyu <city7310@naver.com>
2018-03-15 08:41:26 +0900

    002 - 003

--- 003. Exported Name/main.go
+++ 003. Exported Name/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 )
+
 // 패키지를 import하면 패키지가 외부로 export한 것들에 접근할 수 있음
 // Go에서는 첫 문자가 대문자로 시작하면, 그 패키지를 사용하는 곳에서 접근할 수 있는 exported name이 됨
 // Node.js에서 명시적으로 exports.***나 module.exports를 사용하는 것처럼, Go에서는 첫 문자를 대문자로 둬서 exported name을 표현
