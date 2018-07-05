ff9db98021c6fee7193e1cfb023254dc76631025
JoMingyu <city7310@naver.com>
2018-07-06 08:57:55 +0900

    005

---
+++ 005. Value Type - Boolean & Logical Operator/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Boolean은 true와 false를 나타내는 데 사용되는 1비트 타입
+	a := true
+	b := false
+
+	println(a && b)
+	println(a || b)
+	println(!a)
+	// 3가지의 논리 연산자도 잘 지원되고 있다
+}
