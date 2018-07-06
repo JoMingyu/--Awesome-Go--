d82ca7f79b17bf0b8597079ef0ad3bb97ea1a78c
JoMingyu <city7310@naver.com>
2018-07-06 14:37:33 +0900

    008

---
+++ 008. Constant/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	const A = 123
+	// var 대신 const 키워드를 사용하여 상수 선언
+
+	// A = 456
+	// 상수는 값이 바뀔 수 없으므로 위 statement는 컴파일 오류가 발생한다
+
+	const (
+		SOME  = "Some"
+		THING = "Thing"
+	)
+	// 괄호 나열 방식 선언
+}
